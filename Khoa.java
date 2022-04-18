d07f8c95afeb6de7d7ce04e66811dfe1e7daa4c3
KhoaVo128 <buttskicker128@gmail.com>
2022-04-18 21:41:29 +0700

    2 New Function added

--- Khoa.java
+++ Khoa.java
@@ -2,4 +2,16 @@ public class Khoa{
     private int age;
     private String name;
     public void haylam(){};
+    //mother fucker
+    public void getFucked(){
+        System.out.println("Get fucked bitch");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
 }
